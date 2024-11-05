4c2375fa77cc59f38d2c69fee6458f687eb3d473
thiagosouse <141353723+thiagosouse@users.noreply.github.com>
2024-11-05 13:33:02 -0300

    Add files via upload

---
+++ CAP03/EXERCICIOS-RESOLVIDOS/EXE06/src/br/edu/principal/EXE06.java
@@ -0,0 +1,24 @@
+package br.edu.principal;
+
+import java.util.Scanner;
+
+public class EXE06 {
+
+	public static void main(String[] args) {
+		double salarioBruto, salarioLiquido, gratificacao, imposto;
+		Scanner entrada = new Scanner(System.in);
+
+		System.out.print("Digite o salário do funcionário: ");
+
+		salarioBruto = entrada.nextDouble();
+
+		gratificacao = salarioBruto * 5 / 100;
+
+		imposto = salarioBruto * 7 / 100;
+
+		salarioLiquido = salarioBruto + gratificacao - imposto;
+
+		System.out.println("Novo salário = " + salarioLiquido);
+	}
+
+}
