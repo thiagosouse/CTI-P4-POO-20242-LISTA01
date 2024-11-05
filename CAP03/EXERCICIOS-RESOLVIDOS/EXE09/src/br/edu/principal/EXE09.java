bbe9f2cf6adf0716d055c1990441c9978b080ca1
thiagosouse <141353723+thiagosouse@users.noreply.github.com>
2024-11-05 13:49:52 -0300

    Add files via upload

---
+++ CAP03/EXERCICIOS-RESOLVIDOS/EXE09/src/br/edu/principal/EXE09.java
@@ -0,0 +1,24 @@
+package br.edu.principal;
+
+import java.util.Scanner;
+
+public class EXE09 {
+
+	public static void main(String[] args) {
+		double largura, altura, areaTriangulo;
+		Scanner entrada = new Scanner(System.in);
+
+		System.out.print("Digite a base do triângulo: ");
+
+		largura = entrada.nextDouble();
+
+		System.out.print("Digite a altura do triângulo: ");
+
+		altura = entrada.nextDouble();
+
+		areaTriangulo = (largura * altura) / 2;
+
+		System.out.println("Área do triângulo = " + areaTriangulo);
+	}
+
+}
