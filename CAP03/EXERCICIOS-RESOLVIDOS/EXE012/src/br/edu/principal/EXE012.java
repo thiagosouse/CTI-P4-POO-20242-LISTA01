a3047ad0e20ced481e855d0412c1ea2ccaee7d77
thiagosouse <141353723+thiagosouse@users.noreply.github.com>
2024-11-05 14:50:32 -0300

    Add files via upload

---
+++ CAP03/EXERCICIOS-RESOLVIDOS/EXE012/src/br/edu/principal/EXE012.java
@@ -0,0 +1,30 @@
+package br.edu.principal;
+
+import java.util.Scanner;
+
+public class EXE012 {
+
+	public static void main(String[] args) {
+		double num1, num2, r1, r2;
+		
+		Scanner entrada = new Scanner(System.in);
+   	   
+		System.out.println("Digite o 1º número: ");
+		
+		num1 = entrada.nextDouble();
+		
+		System.out.println("Digite o 2º número: ");
+	
+		num2 = entrada.nextDouble();
+	
+		r1 = Math.pow (num1, num2);
+		
+		r2 = Math.pow (num2, num1);
+	
+		System.out.println("Resposta 1 = " + r1);
+	
+		System.out.println("Resposta 2 = " + r2);
+
+	}
+
+} 
